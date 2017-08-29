5b63590419a54614b0832a350c0f498add21f560
dingjunjie <295846891@qq.com>
2017-08-29 23:14:58 +0800

    update db.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -187,3 +187,63 @@ class _ConnectionCtx(object):
 			with _ConnectionCtx():
 				return func(*args,**kw)
 		return _wrapper
+
+class _TransactionCtx(object):
+	'''
+	_TransactionCtx object that can handle transactions.
+
+	with _TransactionCtx():
+		pass
+	'''
+
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn = False
+		if not _db_ctx.is_init():
+			# needs open a connection first:
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions = _db_ctx.transactions + 1
+		logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
+		return self
+
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions = _db_ctx.transactions - 1
+		try:
+			if _db_ctx.transactions == 0:
+				if exctype is None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.shoule_close_conn:
+				_db_ctx.cleanup()
+
+	def commit(self):
+		global _db_ctx
+		logging.info('commit transaction...')
+		try:
+			_db_ctx.connection.commit()
+			logging.info('commit ok.')
+		except:
+			logging.warning('commit failed.try rollback...')
+			_db_ctx.connection.rollback()
+			logging.warning('rollback ok.');
+			raise 
+
+	def rollback(self):
+		global _db_ctx
+		logging.warning('rollback transaction...')
+		_db_ctx.connection.rollback()
+		logging.info('rollback ok.')
+
+def transaction():
+	'''
+	Create a transaction object so can use with statement:
+
+	with transaction():
+		pass
+	'''
+	return _TransactionCtx()
+
