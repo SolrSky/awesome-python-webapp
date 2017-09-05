2bdac28b11293c925a1d1abe2202e39974812ee3
dingjunjie <295846891@qq.com>
2017-09-05 11:31:07 +0800

    add test_db.py

---
+++ www/test_db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-08-26 14:10:07
+# @Author  : djj (295846891@qq.com)
+# @Link    : ${link}
+# @Version : $Id$
+
+__author__ = "SolrSky"
+
+from models import User, Blog, Comment
+from transwarp import  db
+
+
+db.create_engine(user='www-data', password='www-data', database='awesome')
+
+u = User(name='Test', email='test@example.com', password='123456', image='http://img.qfc.cn/upload/01/certificate/02/0d/705808.jpg')
+
+u.insert()
+
+print "new user id:", u.id
+
+u1 = User.find_first('where email=?', 'test@example.com')
+
+print 'find user\'s name:', u1.name
+
+u1.delete()
+
+u2 = User.find_first('where email=?', 'test@example.com')
+
+print 'find user:', u2
+
